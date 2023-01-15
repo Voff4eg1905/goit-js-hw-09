96e35f0e0cd4e94a9574f6a1a1c85144b7bd55eb
Voff4eg1905 <106484238+Voff4eg1905@users.noreply.github.com>
2023-01-15 13:02:20 +0200

    1 done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+let startPressed = false;
+let ColorChangeId = null;
+const startButtonEl = document.querySelector('button[data-start]');
+const stopButtonEl = document.querySelector('button[data-stop]');
 
+onStartClick = event => {
+  console.log('Start Clicked');
+  if (startPressed) {
+    return;
+  }
+  startPressed = true;
+  ColorChangeId = setInterval(changeColor, 1000);
+  function changeColor() {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }
+};
+
+onStopClick = event => {
+  console.log('Stop Clicked');
+    clearInterval(ColorChangeId);
+  startPressed = false;
+  document.body.style.backgroundColor = 'white';
+};
+startButtonEl.addEventListener('click', onStartClick);
+stopButtonEl.addEventListener('click', onStopClick);
